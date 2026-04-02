c5a582f57901d8873dd3a417be69fed2ee237c19
Erick Su <erickkosysu@gmail.com>
2026-04-01 21:49:58 -0600

    Add missing signOut import in account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useRef, useEffect } from 'react';
-import { useSession } from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
